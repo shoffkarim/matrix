074d834101ba71a4d88ce444e42d403fd18d887f
Karim <k4r1mch1k@gmail.com>
2020-09-24 08:26:24 +0500

    gauss already

--- gauss.js
+++ gauss.js
@@ -13,7 +13,7 @@ let originMatrix = [
 let accuracy = 0.001;
 let f = [10, 1.3, 10, 1.7]
 let originF = [10, 1.3, 10, 1.7];
-errorMessage = "";
+let errorMessage = "";
 
 class Matrix {
   constructor(matrix, f, accuracy, errorMessage){
@@ -30,12 +30,11 @@ class Matrix {
       // todo: check and swaps
 
       del.push(matrix[i][i]) // добавляем в массив делить каждой строки
-      //this.f[i] /= del[i]; // делим элемент f соответствующим делителем
       for (let j = 0; j < this.matrix.length + 1; j++) {
         this.matrix[i][j] /= del[i] // делим каждый элемент строки на делить чтобы получить единицы по диагонали
 
       }
-      // console.log(this.matrix)
+
       for (let j = i + 1; j < this.matrix.length; j++) {
         let coef = this.matrix[j][i] / this.matrix[i][i]
         let right = multiMassiv(this.matrix[i], coef)
@@ -43,57 +42,23 @@ class Matrix {
 
       }
     }
-    //console.log(this.matrix);
+
     // todo: check
   }
 
   reverseStroke(errorMessage){
-    //console.log(this.matrix)
-    // let right = [];
-    // let coef = [];
-    // for (let i = this.matrix.length - 1; i >= 0;  i--) {
-    //   //console.log(this.matrix[i].length)
-    //   for (let j = i - 1; j >=0; j--) {
-    //     right.push(this.matrix[i])
-    //   }
-    // }
     for (let i = this.matrix.length; i >=0;  i--) {
-      //console.log(this.matrix[i].length)
       for (let j = i - 1; j >=0; j--) {
         let coef = this.matrix[j][i];
         if(this.matrix[i]){
-          console.log(coef)
           let right = multiMassiv(this.matrix[i], coef);
           this.matrix[j] = minusMassiv(this.matrix[j], right);
         }
         else{
           i = this.matrix.length
-          console.log(coef)
         }
       }
     }
-    // console.log(right)
-    // for (let i = this.matrix.length - 1; i >= 0; i--) {
-    //   for (let j = i - 1; j >= 0; j--) {
-    //     console.log(coef[j])
-    //   }
-    // }
-    // console.log(coef)
-      // for (let j = i - 1; j >=0; j--) {
-      //   let coef = this.matrix[j][i]
-      //   // let right = multiMassiv(this.matrix[j], coef);
-      //   // this.matrix[j] = minusMassiv(this.matrix[i], right);
-      //   console.log(this.matrix[j][i])
-      //   //console.log(this.matrix[j]);
-
-
-      // for (let j = i - 1; j >= 0; j--) {
-      //   let coef = this.matrix[j][i] / this.matrix[i][i]
-      //   console.log(this.matrix[j][i])
-      //   let right = multiMassiv(this.matrix[i], coef);
-      //   this.matrix[j] = minusMassiv(this.matrix[j], right);
-      // }
-
 
   }
 
@@ -109,7 +74,14 @@ class Matrix {
     }
     this.forwardStroke(this.errorMessage);
     this.reverseStroke(this.errorMessage);
-    console.log(this.matrix);
+    console.log("Решение системы:")
+    for (let i = 0; i < this.matrix.length; i++) {
+      for (let j = 0; j < this.matrix.length + 1; j++) {
+        if(j === 4){
+          console.log("x" + (i + 1) + " = " + this.matrix[i][j])
+        }
+      }
+    }
   }
 }
 
@@ -122,8 +94,7 @@ function minusMassiv(left, right) {
   }
   return left;
 }
-// let arr1 = [1, 2, 3]
-// let coeff = 4
+
 
 function multiMassiv(arr, coef){
   let res = [];
